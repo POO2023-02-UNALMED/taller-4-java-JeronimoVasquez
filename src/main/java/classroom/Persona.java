9132ae05d2346fa79f99067ff3eec967208c38dd
JeronimoVasquez <jeronimovasquez1307@gmail.com>
2023-09-03 22:03:05 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -31,7 +31,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         this.cedula = 1;
         totalPersonas++;
     }
